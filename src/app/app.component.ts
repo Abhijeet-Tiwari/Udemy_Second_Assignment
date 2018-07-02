ca835fd31589dde6af7278cf07c4c5655b869684
Abhijeet Tiwari <abhijeet96tiwari@gmail.com>
2018-07-02 12:28:36 +0530

    DataBinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  userName = 'Abhijeet';
+  checkEmptyUserName:boolean = false;
+  
+  resetInputField() {
+      this.userName = "";
+   }
 }
